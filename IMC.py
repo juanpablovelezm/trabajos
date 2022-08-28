dd1e4f8ed35694c9d8b49ed92fa749b1cc0718f8
juanpablovelezm <111029770+juanpablovelezm@users.noreply.github.com>
2022-08-28 11:40:43 -0500

    Add files via upload

---
+++ IMC.py
@@ -0,0 +1,30 @@
+print("\nvamos a calcular tu indice de masa corporal")
+estatura=float(input("\nIngresa tu estatura en metros\t"))
+peso=float(input("\nIngresa tu peso en kilogramos\t"))
+
+Imc=peso/estatura**2
+
+print(f"\n{round(Imc,1)}")
+
+if Imc < 18.5:
+    tipo_de_peso="Bajo peso"
+
+elif Imc >= 18.5 and Imc<=24.9:
+    tipo_de_peso="Peso normal "
+
+elif Imc >= 25.0 and Imc <= 29.9:
+    tipo_de_peso="Sobre peso"
+
+elif Imc >= 30.0 and Imc <= 34.9:
+    tipo_de_peso="Obesidad clase 1"
+
+elif Imc >= 35.0 and Imc <= 39.9:
+    tipo_de_peso="Obesidad clase 2"
+
+elif Imc >= 40.0:
+    tipo_de_peso="Obesidad"
+
+print(f"\nTienes {tipo_de_peso}")
+
+
+
